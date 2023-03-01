d724d01693be2fdcc6a31937becff062a7fecf67
MohamedAbdElwahabOka <boka22986@gmail.com>
2023-03-01 17:32:13 +0200

    js project

---
+++ Js-project/javascript/Modal Login Form.js
@@ -0,0 +1,8 @@
+
+
+function viewLogInTab() {
+    document.getElementById('id01').style.display = 'block'
+}
+function closeLogInTab() {
+    document.getElementById('id01').style.display = 'none'
+}
